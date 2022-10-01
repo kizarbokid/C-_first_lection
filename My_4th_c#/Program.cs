e3806303292956ba20359cad54a413efa169a404
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-01 16:30:42 +0900

    task 4 done

---
+++ My_4th_c#/Program.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+if(username=="123")
+{
+    Console.WriteLine("Hello again fello!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
